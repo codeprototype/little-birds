0fe199dd064eb107d587d5898ac82bfa35d44950
codeprototype <maaz.abdullah@techmojo.in>
2024-02-21 15:56:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
+
+app.use((req, res, next) => {
+  logger.info({
+    method: req.method,
+    url: req.url,
+    statusCode: res.statusCode,
+    responseTime: Date.now() - req.startTime,
+  });
+  next();
+});
+app.get("/", (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    message: "Welcome to Lord Varys Little Birds Service",
+    data: "NA",
+  });
+});
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started at PORT: ${PORT}`);
+});
