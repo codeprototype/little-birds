7d9247aa235f5f140f67397d3e4c836fd9e93bd5
codeprototype <maaz.abdullah@techmojo.in>
2024-02-21 18:06:26 +0530

    upload and get file for aws

--- castleblack.ts
+++ castleblack.ts
@@ -1,6 +1,64 @@
+import AWS from "aws-sdk";
+import fs from "fs";
+const config = process.env;
 
-const castleBlack =async (params:type) => {
-    
+AWS.config.update({
+  accessKeyId: config.AWS_ACCESS_KEY,
+  secretAccessKey: config.AWS_SECRET_KEY,
+  region: config.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+// const castleBlack =async (params:type) => {
+
+function uploadFileToS3Bucket(
+  fileToBeUploaded: string,
+  bucket_name: string,
+  filename: string,
+  ContentType: any = null
+) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileToBeUploaded, function (error, file_buffer) {
+      if (error) {
+        return reject(error);
+      }
+      var params = {
+        Bucket: bucket_name,
+        Key: filename,
+        Body: file_buffer,
+        ACL: "public-read",
+        ContentType: ContentType || "application/pdf",
+      };
+      s3.putObject(params, function (err, data) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(true);
+      });
+    });
+  });
 }
 
-export default castleBlack
+const s3UrlToBuffer = (bucket: string, url: string) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      s3.getObject(
+        {
+          Bucket: bucket,
+          Key: url,
+        },
+        function (err, data) {
+          if (err) return reject(err);
+          else return resolve(data);
+        }
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+export default {
+  uploadFileToS3Bucket,
+  s3UrlToBuffer,
+};
