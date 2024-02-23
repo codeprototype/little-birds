b89f24ff263f71c99c25e0512fe21e09a773470c
codeprototype <maaz.abdullah@techmojo.in>
2024-02-23 13:04:36 +0530

    signed url changes

--- castleblack.ts
+++ castleblack.ts
@@ -1,24 +1,24 @@
 import AWS from "aws-sdk";
-import 'dotenv/config';
+import "dotenv/config";
 const config = process.env;
 
 export interface awsData {
-  IsTruncated:    boolean;
-  Contents:       Content[];
-  Name:           string;
-  Prefix:         string;
-  MaxKeys:        number;
+  IsTruncated: boolean;
+  Contents: Content[];
+  Name: string;
+  Prefix: string;
+  MaxKeys: number;
   CommonPrefixes: any[];
-  KeyCount:       number;
+  KeyCount: number;
 }
 
 export interface Content {
-  Key:               string;
-  LastModified:      string;
-  ETag:              string;
+  Key: string;
+  LastModified: string;
+  ETag: string;
   ChecksumAlgorithm: any[];
-  Size:              number;
-  StorageClass:      string;
+  Size: number;
+  StorageClass: string;
 }
 
 AWS.config.update({
@@ -32,7 +32,7 @@ const s3 = new AWS.S3();
 const uploadFileToS3 = async (
   file: any,
   name: any,
-  bucket:any = null,
+  bucket: any = null,
   nocache = null
 ) => {
   return new Promise(async (resolve, reject) => {
@@ -47,7 +47,7 @@ const uploadFileToS3 = async (
       ContentType: file.mimetype,
       ACL: "public-read",
     };
-    console.log("????", objectParams)
+    console.log("????", objectParams);
     if (nocache) {
       objectParams["CacheControl"] = "no-cache";
     }
@@ -66,19 +66,29 @@ const uploadFileToS3 = async (
 const listS3File = (bucket: any, url?: string) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const data:any = await s3.listObjectsV2(
-        {
-          Bucket: bucket
-        });
-        return resolve(data.Contents)
-        
+      const data = await s3.listObjectsV2({ Bucket: bucket }).promise();
+
+      const keys = data.Contents?.map((obj) => obj.Key) || [];
+
+      if (keys.length === 0) {
+        console.log("No objects found in the bucket.");
+        return [];
+      }
+
+      const signedUrlsPromises = keys.map((key) =>
+        s3.getSignedUrlPromise("getObject", {
+          Bucket: bucket,
+          Key: key,
+          Expires: 60 * 5,
+        })
+      );
+
+      const signedUrls = await Promise.all(signedUrlsPromises);
+      return resolve(signedUrls);
     } catch (err) {
       reject(err);
     }
   });
 };
 
-export  {
-  uploadFileToS3,
-  listS3File,
-};
+export { uploadFileToS3, listS3File };
